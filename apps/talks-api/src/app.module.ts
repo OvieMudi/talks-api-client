fbda1fb6bc74662fc3efd14a3d0db6704b578486
Ovie Udih <oviisaacs@gmail.com>
2023-04-25 00:28:53 +0100

    chore: serve client static files
    - point server to client static files
    - add logger middleware

--- apps/talks-api/src/app.module.ts
+++ apps/talks-api/src/app.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { UserModule } from './user/user.module';
 import { TalkModule } from './talk/talk.module';
+import { ChatappModule } from './chatapp/chatapp.module';
+import { AppLoggerMiddleware } from './middleware/AppLoggerMiddleware';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { ChatappModule } from './chatapp/chatapp.module';
 
 @Module({
   imports: [
     UserModule,
     TalkModule,
-    DatabaseModule,
     ChatappModule,
+    DatabaseModule,
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'talks-client', 'dist'),
+    }),
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AppLoggerMiddleware).forRoutes('*');
+  }
+}
